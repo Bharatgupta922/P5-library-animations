c1254597c4788d4879dd748533b91a3ff7685b07
Bharatgupta922 <gbharat922@gmail.com>
2020-09-10 03:21:43 +0530

    added a constructor

--- bubblesDropping/sketch.js
+++ bubblesDropping/sketch.js
@@ -6,30 +6,7 @@ var bubbles = [];
 function setup() {
     createCanvas(1525, 725);
 for(var i = 0 ; i <40 ; ++i)
-    bubbles[i] = {
-        x: random(0,width),
-        y: random(0,height),
-        display: function () {
-            noStroke();
-            // stroke(255);
-            strokeWeight(2);
-            fill(random(0,255),0, random(0,255));
-            // noFill();
-            ellipse(this.x, this.y, 24,24);
-        },
-        move: function () {
-            this.x += random(-1,1);
-            this.y += random(-1,1);
-        }
-        // bounce: function () {
-        //     if (this.x > width || this.x < 0) {
-        //         this.xspeed *= -1;
-        //     }
-        //     if (this.y > height || this.y < 0) {
-        //         this.yspeed *= -1;
-        //     }
-        // }
-    };
+    bubbles[i] = new Bubbles();
 print(bubbles);
 
 }
@@ -40,4 +17,23 @@ for(var i = 0 ; i < bubbles.length ; ++i){
     bubbles[i].move();
 }
     // ball.bounce();
+}
+
+
+function Bubbles(){
+this.x = random(0,width);
+this.y = random(0,height);
+
+this.display = function(){
+    noStroke();
+    // stroke(255);
+    strokeWeight(2);
+    fill(random(0,255),0, random(0,255));
+    // noFill();
+    ellipse(this.x, this.y, 24,24);
+}
+this.move = function(){
+    this.x += random(-1,1);
+    this.y += random(-1,1);
+}
 }
