227e7eb813a9a73ce07d368f231ef1f6d92fec75
Bharatgupta922 <gbharat922@gmail.com>
2020-09-10 01:41:21 +0530

    star with red box patching animation using p5

---
+++ StarWredbox/sketch.js
@@ -0,0 +1,30 @@
+/// <reference path="./p5.global-mode.d.ts" />
+
+let x = 800;
+let y = 400;
+let extraCanvas;
+function setup() {
+    createCanvas(1525, 725);
+    extraCanvas = createGraphics(1525, 725);
+    extraCanvas.clear();
+    background(0);
+}
+function draw() {
+    background(0);
+
+    x += random(-5, 5);
+    y += random(-5, 5);
+
+    // if (mouseIsPressed) {
+    extraCanvas.fill(255, 150);
+    extraCanvas.noStroke();
+    let starX = random(width);
+    let starY = random(height);
+    extraCanvas.ellipse(starX, starY, 15, 15);
+    // }
+    image(extraCanvas, 0, 0);
+    fill(255, 0, 0);
+    stroke(255);
+    rectMode(CENTER);
+    rect(x, y, 30, 30);
+}
