739373c6f1c1a8c5a2cb36a432af578b615cac23
Bharatgupta922 <gbharat922@gmail.com>
2020-09-10 23:22:52 +0530

    updated

--- bubblesDropping/sketch.js
+++ bubblesDropping/sketch.js
@@ -11,9 +11,16 @@ function draw() {
     for (var i = bubbles.length -1 ; i >=0 ; --i) {
         bubbles[i].update();
         bubbles[i].display();
+        for(var j = 0 ; j < bubbles.length - 1 ; ++j){
+            if(i!=j && bubbles[i].intersect(bubbles[j])){
+                bubbles[i].changeColor();
+                bubbles[j].changeColor();
+            }
+        }
         if(bubbles[i].isFinished()){
         bubbles.splice(i,1);
         }
+
     }
     // ball.bounce();
     // if (bubbles.length > 50) {
